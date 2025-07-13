55d6ad9adf6191b883732efe3505184e65d37594
pzaydulin <pzaydulin@gmail.com>
2025-07-13 01:20:57 -0700

    feat: update TypeScript and Cypress tsconfig (fixed conflict with aliases)

--- src/app/pages/client/booking/components/booking-box/booking-box.component.ts
+++ src/app/pages/client/booking/components/booking-box/booking-box.component.ts
@@ -1,12 +1,17 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit } from '@angular/core';
-import { BookingVariantFirstImagePipe } from "../../pipes/booking-variant-first-image.pipe";
-import { BackgroundImagePipe } from "../../../../../shared/pipes/background-image.pipe";
+import {
+  ChangeDetectionStrategy,
+  Component,
+  inject,
+  OnInit,
+} from '@angular/core';
+import { BackgroundImagePipe } from '@app/shared/pipes/background-image.pipe';
 import { Observable } from 'rxjs';
 import { BookingVariant } from '@app/models/booking.interface';
-import { BookingService } from '../../data-access/booking.service';
 import { CommonModule } from '@angular/common';
 import { NavigationService } from '@app/data-access/navigation.service';
 import { NavigationPath } from '@app/models/navigation.interface';
+import { BookingService } from '@app/pages/client/booking/data-access/booking.service';
+import { BookingVariantFirstImagePipe } from '@app/pages/client/booking/pipes/booking-variant-first-image.pipe';
 
 @Component({
   selector: 'app-booking-box',
@@ -19,7 +24,8 @@ export class BookingBoxComponent implements OnInit {
   bookingVariant$!: Observable<BookingVariant>;
 
   private readonly bookingService: BookingService = inject(BookingService);
-  private readonly navigationService: NavigationService = inject(NavigationService);
+  private readonly navigationService: NavigationService =
+    inject(NavigationService);
 
   ngOnInit(): void {
     this.bookingVariant$ = this.bookingService.bookingVariant$;
@@ -27,7 +33,6 @@ export class BookingBoxComponent implements OnInit {
 
   onClick(bookingVariant: BookingVariant): void {
     if (bookingVariant.firstRoom?.id) {
-      console.log('Booking variant clicked:', bookingVariant.firstRoom.id);
       void this.navigationService.navigateByUrl(NavigationPath.RoomPage, {
         id: bookingVariant.firstRoom?.id,
       });
